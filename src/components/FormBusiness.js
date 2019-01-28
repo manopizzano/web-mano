ba63b79ddb5baacc54963f0a9048900b83f9f6fa
mittnavnermike <mikepatterson@Mikes-MBP-2.heydays.local>
2019-01-28 08:42:55 +0100

    updated form

--- src/components/FormBusiness.js
+++ src/components/FormBusiness.js
@@ -160,46 +160,6 @@ export default class FormBusiness extends Component {
           )}
           <button onClick={this.resetForm}>Reset form</button>
         </form>
-        <form name="contact" action="/" method="POST" data-netlify="true">
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Role:{' '}
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-        {/* <!-- A little help for the Netlify post-processing bots --> */}
-        <form
-          name="businessform"
-          netlify="true"
-          netlify-honeypot="bot-field"
-          hidden
-        >
-          <input type="text" name="companyName" />
-          <input type="email" name="email" />
-          <textarea name="desc" />
-        </form>
       </div>
     )
   }
