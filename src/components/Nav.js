4efc3fdf83df36d18a47f9c9a7f47fe7e30bf691
hmdall <40295166+hmdall@users.noreply.github.com>
2019-06-14 12:23:08 +0200

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -34,7 +34,7 @@ export default class Nav extends Component {
                   rel="noopener noreferrer"
                   className="Nav__col Nav__col--order"
                 >
-                  Bestill
+                  ORDERRRRRR
                 </a>
                 <button
                   className="Nav__col Nav__col--menu"
